09504e67d5078858c3a48759f3c7094b844df395
Kannan <kannan-inbox@outlook.com>
2020-03-04 14:34:09 +0530

    Completed  Streaming application

--- Client/src/components/streams/StreamShow.js
+++ Client/src/components/streams/StreamShow.js
@@ -1,16 +1,43 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
+import flv from "flv.js";
 class StreamShow extends Component {
-  componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
   }
+  componentDidMount = () => {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  };
+  componentDidUpdate = () => {
+    this.buildPlayer();
+  };
+  componentWillUnmount = () => {
+    this.player.destroy();
+  };
+  buildPlayer = () => {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+    //this.player.play();
+  };
   render() {
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls={true} />
         <h1>{this.props.stream.title}</h1>
         <h4>{this.props.stream.description}</h4>
       </div>
